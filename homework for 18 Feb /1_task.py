ae85efc962fba82c60fe1f9ba0fff04f5634ec9d
Anna Arkhipova <arkhipova-1997@yandex.ru>
2019-02-23 15:38:24 +0300

    hmw_added

---
+++ homework for 18 Feb /1_task.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 11:54:44 2019
+
+@author: annaarkx
+"""
+
+import math
+
+a = 3 # значение первого катета
+b = 4 # значение второго катета
+
+c = a**2 + b**2
+c = math.sqrt(c)
+
+print('Гипотенуза равна {}'.format(c))
---
+++ homework for 25 Feb/practice - py3.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+class Practice:
+    def is_prime():
+        for i in range(0,1001):
+            if not i % 2:
+                print (i, ':', False)
+            else:
+                print(i, ':', True)
+        return
+    is_prime()
+
+
+
+if __name__ == '__main__':
+    Practice()
