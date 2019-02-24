e6a95fe8658769a5d03c2933b9444a0becb82efa
Anna Arkhipova <arkhipova-1997@yandex.ru>
2019-02-24 21:28:23 +0300

    contents

--- Draft.py
+++ Draft.py
@@ -5,3 +5,46 @@
 Draft
 '''
 
+class Money:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def penny_count(self):
+        return self.x * 100 + self.y
+
+    def __add__(self, other):
+        return self.penny_count() + other.penny_count()
+        # сложение - penny count 330  и penny count 495
+
+    def __sub__(self, other):
+        return self - other
+
+
+
+m1 = Money(3, 30)
+penny = m1.penny_count()
+print('m1 в копейках равен {}'.format(penny))
+
+    def __int__(self):
+        return self.m1
+
+print(type(m1))
+
+m2 = Money(4, 95)
+
+m3 = m1 + m2
+
+print('Мы получили {}'.format(m3))
+# на экране появится "Мы получили 8руб. 25 коп."
+
+
+#
+# if m1 == m2:
+#     print('{} и {} равны'.format(m1, m2)
+# elif m1 > m2:
+#     print('{} больше чем {}'.format(m1, m2)
+# else:
+#     print('{} меньше чем {}'.format(m1, m2)
--- homework for 25 Feb/practice - py3.py
+++ homework for 25 Feb/practice - py3.py
@@ -2,18 +2,43 @@
 
 
 
-
 class Practice:
     def is_prime():
-        for i in range(0,1001):
-            if not i % 2:
-                print (i, ':', False)
-            else:
-                print(i, ':', True)
+        for a in range(2, 1001):
+            prime = True
+            for i in range(2, a):
+                if a % i == 0:
+                    prime = False
+            if prime:
+                print (a, '- prime')
         return
     is_prime()
 
 
+    def modul_chisla():
+        n = int(input('Put some number:'))
+        if n >= 0:
+            print(n)
+        else:
+            print(-n)
+        return
+    modul_chisla()
+
+
+    def wave():
+        word = 'hello'
+        if word.startswith(word[0].upper()) == False:
+            update = word[0].upper()
+            a = word[1:]
+            print(update + a)
+
+
+        # wave("hello") = > ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+
+
+        return
+    wave()
 
 if __name__ == '__main__':
     Practice()
