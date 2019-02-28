5ecf61c7ed94ecf4de8114e7827de87b36f5d32a
Anna Arkhipova <arkhipova-1997@yandex.ru>
2019-02-28 10:55:14 +0300

    new_hmw

--- Draft.py
+++ Draft.py
@@ -5,21 +5,5 @@
 Draft
 '''
 
-def to_title(name):
-     # y = ''
-     # flag = False
-     # for k in name:
-     #      print(k)
-     #      # y += k
-     #      if k == ' ':
-     #           flag = True
-     #      elif flag is True:
-     #          print(k.upper())
-     #          flag = False
-
-
-
-
-
 
 
