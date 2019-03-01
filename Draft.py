ce762d1593164ea3cce6c689fe0b91a04078b1e2
Anna Arkhipova <arkhipova-1997@yandex.ru>
2019-03-01 22:35:04 +0300

    correc

--- Draft.py
+++ Draft.py
@@ -4,12 +4,3 @@
 '''
 Draft
 '''
-
-import itertools
-
-e = ([1,2,3], [4,5], [6,7])
-
-merged = list(itertools.chain.from_iterable(e))
-
-
-print(merged)
--- homework for 4 Mar/tasks.py
+++ homework for 4 Mar/tasks.py
@@ -2,10 +2,9 @@
 
 import itertools
 
+def func(e):
+    merged = list(itertools.chain.from_iterable(e))
+    return merged
+
 e = ([1,2,3], [4,5], [6,7])
-
-merged = list(itertools.chain.from_iterable(e))
-
-
-print(merged)
-
+print(func(e))
