b21b6f7798c7037860d36ad0703d9e3bb1bf4e3c
Anna Arkhipova <arkhipova-1997@yandex.ru>
2019-03-03 14:23:47 +0300

    anozertask

--- homework for 4 Mar/task2.py
+++ homework for 4 Mar/task2.py
@@ -1,5 +1,25 @@
-#
-# Функция принимает массив (['hello', 'i', 'write', 'cool', 'code'])
-# и возвращать массив из элементов, у которых длина не меньше пяти (['hello', 'world'])
 
 
+
+from itertools import compress
+
+
+def bar(l):
+    p = compress(l, [1, 0, 1, 0, 0, 0])
+    return list(p)
+
+
+l = (['hello', 'i', 'write', 'cool', 'code'])
+print(bar(l))
+
+
+
+
+
+# l = (['hello', 'i', 'write', 'cool', 'code'])
+# b = [False]
+# for i in l:
+#     if len(i) == 5:
+#         b = [True]
+#         data = list(compress(i, b))
+#         print(data)  # ['A', 'C', 'D']
